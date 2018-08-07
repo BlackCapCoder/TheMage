8233f44271ae691fce4bcd1178abf344f30f341f
BlackCap <mailhph@gmail.com>
2018-08-07 18:08:49 +0200

    added ring to log

--- intro.js
+++ intro.js
@@ -1,10 +1,10 @@
-
 var boxEvent = (function () {
   let ringFound = false;
   return val => {
     if (ringFound || val < 10) return;
     ringFound = true;
     document.querySelector('#ring').classList.remove('hidden');
+    log("You found a ring under a box");
   };
 })();
 

4c220c0219782ba4dc4c197579097fcf23042ba9
BlackCap <mailhph@gmail.com>
2018-08-07 05:22:32 +0200

    ring

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 resources = {};
 
-function mkResource (name, val=0, els=[]) {
+function mkResource (name, val=0, els=[], listeners=[]) {
   const r = (resources[name] = new Proxy (
-    { els: els },
+    { els: els
+    , listeners: listeners
+    },
     { set: function (obj, prop, val) {
         if (prop != 'val') return;
         obj[prop] = val;
         for (let e of obj.els) e.innerText = val;
+        for (let l of obj.listeners) l(val);
       }
     }
   ));
 
-  r.val = val;
+  r.val = val; // To initialize the graphics
   return r;
 }
 
 window.addEventListener('load', _ => {
-  mkResource('boxesMoved', 0, [document.querySelector('.boxes-moved')]);
+  mkResource
+    ( 'boxesMoved'
+    , 0
+    , [ document.querySelector('.boxes-moved') ]
+    , [ boxEvent ]
+    );
 });
+
+var boxEvent = (function () {
+  let ringFound = false;
+  return val => {
+    if (ringFound || val < 10) return;
+    ringFound = true;
+    alert("You found a ring!");
+  };
+})();
 
 function onBoxMoved () {
   resources.boxesMoved.val++;

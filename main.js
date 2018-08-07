cec125e9692ae84332ae8ec411540dce4f56adce
BlackCap <mailhph@gmail.com>
2018-08-07 06:47:58 +0200

    functional tabs

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', _ => {
+  const tabs = document.querySelectorAll("#tabs #header .tab");
+
+  for (let t of tabs) {
+    const id = t.getAttribute('data-tab-id');
+    t.onclick = _ => {
+      document.querySelector("#tabs #header .tab.active").classList.remove('active');
+      t.classList.add('active');
+      document.querySelector("#tabs #content .page.active").classList.remove('active');
+      document.querySelector("#tabs #content .page[data-tab-id='"+id+"']").classList.add('active');
+    };
+  }
+});

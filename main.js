a0f2cf8c5314598b25f2fb7582fdb7c6c91c31d4
BlackCap <mailhph@gmail.com>
2018-08-07 05:13:10 +0200

    boxes

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-boxesMoved = 0;
+resources = {};
+
+function mkResource (name, val=0, els=[]) {
+  return (resources[name] = new Proxy (
+    { val: val, els: els },
+    { set: function (obj, prop, val) {
+        obj[prop] = val;
+        for (let e of obj.els) e.innerText = val;
+      }
+    }
+  ));
+}
+
+window.addEventListener('load', _ => {
+  const r = mkResource('boxesMoved', 0,
+    [ document.querySelector('.boxes-moved') ]
+  );
+  r.val = r.val;
+});
 
 function onBoxMoved () {
-  document.querySelector('.boxes-moved').innerText = ++boxesMoved;
+  resources.boxesMoved.val++;
 }

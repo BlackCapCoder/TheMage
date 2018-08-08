180b3c59b0427947ddf516440c10e88c60abbe01
BlackCap <mailhph@gmail.com>
2018-08-08 05:05:11 +0200

    research

--- intro.js
+++ intro.js
@@ -16,3 +16,24 @@ var boxEvent = (function () {
 function onBoxMoved () {
   resources.boxesMoved.val++;
 }
+
+var introStage = 0;
+function introButtonClicked () {
+  const prg = document.querySelector('#ring-intro-btn .progress');
+  let progress = 0;
+
+  const si = setInterval(function () {
+    progress = Math.min(progress+1, 100);
+    prg.style.width = progress + '%';
+    if (progress < 100) return;
+    clearInterval(si);
+    prg.style.width = 0;
+    document.querySelector('#ring-intro-btn .label').innerText =
+      [ "Turn it right side up"
+      , "Stare at it"
+      , "Wipe the stone"
+      , "Wave your hand around"
+      , "The ring zapped with magic!"
+      ][introStage++];
+  }, 1000/60);
+}

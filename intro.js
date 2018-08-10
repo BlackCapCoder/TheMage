c2e20d500e4cccb8d90f2f24c5efba45b37cd203
KiriKaz <igkepsi@gmail.com>
2018-08-10 13:28:00 -0300

    Research fix.
    You can now only have one 'research' in progress, and it doesn't allow you to go past "The ring zapped with magic!". Simple if statements.
    
    Also the tab works properly now.

--- intro.js
+++ intro.js
@@ -9,33 +9,37 @@ var boxEvent = (function () {
     document.querySelector('#sidebar').classList.remove('hidden');
     document.querySelector('#tabs #header').classList.remove('hidden');
     document.querySelector('#tab2').innerText = "Research";
-    document.querySelector('#tab2').classList.remove('uHidd');
+    document.querySelector('#tab2').classList.remove('unClick');
     document.querySelector('log').classList.remove('hidden');
-    log("You found a ring under a box");
+    log("You found a ring under a box!");
   };
 })();
 
 function onBoxMoved () {
   resources.boxesMoved.val++;
 }
-
+var inProgress = 0;
 var introStage = 0;
 function introButtonClicked () {
-  const prg = document.querySelector('#ring-intro-btn .progress');
-  let progress = 0;
+  if (inProgress){return;} else if (introStage==5){return;} else { 
+  	const prg = document.querySelector('#ring-intro-btn .progress');
+  	let progress = 0;
+  	inProgress = 1;
 
-  const si = setInterval(function () {
-    progress = Math.min(progress+1, 100);
-    prg.style.width = progress + '%';
-    if (progress < 100) return;
-    clearInterval(si);
-    prg.style.width = 0;
-    document.querySelector('#ring-intro-btn .label').innerText =
+  	const si = setInterval(function () {
+      progress = Math.min(progress+1, 100);
+      prg.style.width = progress + '%';
+      if (progress < 100) return;
+      clearInterval(si);
+      prg.style.width = 0;
+      inProgress = 0;
+      document.querySelector('#ring-intro-btn .label').innerText =
       [ "Turn it right side up"
       , "Stare at it"
       , "Wipe the stone"
       , "Wave your hand around"
       , "The ring zapped with magic!"
       ][introStage++];
-  }, 1000/60);
+  	}, 1000/60);
+  }
 }
--- main.js
+++ main.js
@@ -4,10 +4,14 @@ window.addEventListener('load', _ => {
   for (let t of tabs) {
     const id = t.getAttribute('data-tab-id');
     t.onclick = _ => {
-      document.querySelector("#tabs #header .tab.active").classList.remove('active');
-      t.classList.add('active');
-      document.querySelector("#tabs #content .page.active").classList.remove('active');
-      document.querySelector("#tabs #content .page[data-tab-id='"+id+"']").classList.add('active');
+      if (t.classList.contains('unClick')) {
+        return;
+      } else {
+        document.querySelector("#tabs #header .tab.active").classList.remove('active');
+        t.classList.add('active');
+        document.querySelector("#tabs #content .page.active").classList.remove('active');
+        document.querySelector("#tabs #content .page[data-tab-id='"+id+"']").classList.add('active');
+      }
     };
   }
 });
